d4c591305c206e5ec15b299b9c8af5039813877b
lxj <lixj_zj@163.com>
2019-04-16 00:01:15 +0800

    add weibo spider

---
+++ Spider/weibo/weiboAPI.py
@@ -0,0 +1,13 @@
+"""
+ !/usr/bin/env python3.6
+ -*- coding: utf-8 -*-
+ --------------------------------
+ Description :
+ --------------------------------
+ @Time    : 2019/4/14 18:40
+ @File    : weiboAPI.py
+ @Software: PyCharm
+ --------------------------------
+ @Author  : lixj
+ @contact : lixj_zj@163.com
+"""
