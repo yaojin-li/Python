cfe80f31396f1d89d604e207eb4dd0f7b36e010e
lxj <lixj_zj@163.com>
2019-04-29 23:31:47 +0800

    mod weiboapi test

--- Spider/weibo/weiboAPI.py
+++ Spider/weibo/weiboAPI.py
@@ -13,34 +13,32 @@
 """
 
 from weibo import APIClient
-import webbrowser
 
+# 1.配置
 APP_KEY = '4073142975'
 APP_SECRET = '6e8a766757e8ae11b06f0e0bfc26b291'
 CALLBACK_URL = 'http://apps.weibo.com/heyshheyou'  # 回调授权页面，用户完成授权后返回的页面
+
+# 2.调用APIClient生成client实例
 client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
 
-url = client.get_authorize_url()  # 得到授权页面的url
-# webbrowser.open_new(url)
+# 3.得到授权页面的url
+url = client.get_authorize_url()
 print(url)
-code = '7cffcf7f949726b2ab2ed753df994b57'
 
-client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
-r = client.request_access_token(code)
-client.set_access_token(r.get('access_token'), r.get('expires_in'))
+# 4.点击访问url，在浏览器端获得code
+code = '6ecdbf350f0680a6f00cc8c34ae721a6'
+req = client.request_access_token(code)
+client.set_access_token(req.get('access_token'), req.get('expires_in'))
 
-print(r)
-
-access_token = r.access_token
-expires_in = r.expires_in
-client.set_access_token(access_token, expires_in)
-
-#print client.statuses__public_timeline()
+# 5.调用微博普通读取接口，返回最新的公共微博。
+# 接口详情见 https://open.weibo.com/wiki/2/statuses/public_timeline
 statuses = client.statuses__public_timeline()['statuses']
-length = len(statuses)
-#输出了部分信息
-for i in range(0,length):
-    print(u'昵称：'+statuses[i]['user']['screen_name'])
-    print(u'简单介绍：'+statuses[i]['user']['description'])
-    print(u'位置：'+statuses[i]['user']['location'])
-    print(u'微博：'+statuses[i]['text'])
+print(len(statuses))
+# 6.输出部分信息
+for i in range(0, len(statuses)):
+    print(u'昵称：' + statuses[i]['user']['screen_name'])
+    print(u'简单介绍：' + statuses[i]['user']['description'])
+    print(u'位置：' + statuses[i]['user']['location'])
+    print(u'微博：' + statuses[i]['text'])
+    print(statuses[i])
