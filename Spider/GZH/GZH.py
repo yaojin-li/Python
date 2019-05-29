a25c8f4688e2b9d40355767c73331ecab7b1088b
lixj-zj <42533609+lixj-zj@users.noreply.github.com>
2019-05-29 19:19:44 +0800

    Update GZH.py

--- Spider/GZH/GZH.py
+++ Spider/GZH/GZH.py
@@ -3,12 +3,12 @@
  -*- coding: utf-8 -*-
  --------------------------------
  Description :
- 1. 爬去文章
+ 1. 爬取文章
  2. 下载图片
  3. 替换图片
- 4. 输出pdf
+ 4. 输出html
  --------------------------------
- @Time    : 2018/12/21 21:22
+ @Time    : 2019/5/29 11:54
  @File    : GZH.py
  @Software: PyCharm
  --------------------------------
@@ -22,15 +22,21 @@ import logging
 import random
 import re
 import os
-import configure.userAgent
+import userAgent
+import time
+import uuid
 
 # logging.basicConfig函数对日志的输出格式及方式做相关配置
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+                    format='%(asctime)s - %(filename)s[line:%(lineno)d] '
+                           '- %(levelname)s: %(message)s')
 
 
-# 选取随机的IP地址
 def getRandomIP():
+    """
+    获取随机的IP地址
+    :return:
+    """
     with open("ipPool.txt", "r") as f:  # 构建IP池
         content = f.read()
         contList = content.split("', '")
@@ -38,10 +44,17 @@ def getRandomIP():
         random_ip = random.choice(ipList)
         proxy_ip = "http://" + random_ip
         proxies = {"http": proxy_ip}
+        logging.info("random ip is {}".format(proxies))
     return proxies
 
 
 def downloadImg(imgLinkList, imgPath):
+    """
+    下载所有图片
+    :param imgLinkList:
+    :param imgPath:
+    :return:
+    """
     if not os.path.exists(imgPath):
         os.makedirs(imgPath)
     os.chdir(imgPath)  # 切换下载图片的目录
@@ -51,39 +64,65 @@ def downloadImg(imgLinkList, imgPath):
         try:
             with open(str(imgNum) + "." + suffix, "wb") as f:
                 f.write(img.content)
-                logging.info("Download %s th img succeed!" % str(imgNum))
+                logging.info("Download {imgNum} th img succeed!".format(imgNum=str(imgNum)))
         except Exception as e:
             logging.error(str(e))
 
 
 def getImgLinkList(url):
-    req = requests.get(url, headers=headers, proxies=proxies)
-    struct = etree.HTML(req.text)
-    # 获取所有图片地址
-    xPath = "//img/@data-src"  # 匹配任意深度含有data-src熟悉的图片，获取链接
-    imgLinkList = struct.xpath(xPath)
-    return imgLinkList
+    """
+    获取所有图片链接
+    :param url:
+    :return:
+    """
+    try:
+        req = requests.get(url, headers=headers, proxies=proxies)
+        struct = etree.HTML(req.text)
+        # 获取所有图片地址
+        xPath = "//img/@data-src"  # 匹配任意深度含有data-src熟悉的图片，获取链接
+        imgLinkList = struct.xpath(xPath)
+        logging.info("get img link list succeed!")
+        return imgLinkList
+    except Exception as e:
+        logging.error(str(e))
 
 
 def downloadHtml(url, htmlPath):
+    """
+    下载html页面，命名为文章名.html
+    :param url:
+    :param htmlPath:
+    :return:
+    """
+    if not os.path.exists(htmlPath):
+        os.makedirs(htmlPath)
+    os.chdir(htmlPath)  # 切换根目录
     try:
         req = requests.get(url, headers=headers, proxies=proxies)
         struct = etree.HTML(req.text)
         xPath = "//h2/text()"
         title = struct.xpath(xPath)
         htmlName = title[0].replace("\\n", "").strip()
-        with open(htmlPath + htmlName + ".html", "w+", encoding="utf-8") as f:
+        with open(htmlName + ".html", "w+", encoding="utf-8") as f:
             f.write(req.text)
+        logging.info("download old html succeed!")
         return htmlName
     except Exception as e:
         logging.error(str(e))
 
 
 def replaceImg(htmlPath, htmlName, imgPath):
+    """
+    替换图片
+    :param htmlPath:
+    :param htmlName:
+    :param imgPath:
+    :return:
+    """
     pathList = os.listdir(imgPath)
     pathList.sort(key=lambda x: int(x.split(".")[0]))  # 顺序读取
-
-    with open(htmlPath + htmlName + ".html", "r+", encoding="utf-8") as f:
+    os.chdir(htmlPath)
+    with open(htmlName + ".html", "r+", encoding="utf-8") as f:
         html = f.read()
         pattern = r'<img .*?/>'
         imgre = re.compile(pattern)
@@ -96,35 +135,73 @@ def replaceImg(htmlPath, htmlName, imgPath):
             if html.__contains__(img):
                 newHtml = html.replace(img, newImgTag)
                 html = newHtml
+    logging.info("replace img succeed!")
     return html
 
 
+def delFile(path, htmlName):
+    """
+    删除指定文件
+    :param path:
+    :param htmlName:
+    :return:
+    """
+    try:
+        os.remove(path + os.path.altsep + htmlName + ".html")
+        logging.info("remove file {htmlName}.html done!".format(htmlName=htmlName))
+    except Exception as e:
+        logging.error(str(e))
+
+
 def writeImgToNewHTML(newHtmlPath, html, htmlName):
-    with open(newHtmlPath + htmlName + ".html", "w+", encoding="utf-8") as f:
-        f.write(html)
+    """
+    重写文件中的图片，生成新的html
+    :param newHtmlPath:
+    :param html:
+    :param htmlName:
+    :return:
+    """
+    os.chdir(newHtmlPath)
+    try:
+        # 直接覆盖原来没有图片的文件
+        with open(htmlName + ".html", "w+", encoding="utf-8") as f:
+            f.write(html)
+        logging.info("rewrite img to new html succeed!")
+    except Exception as e:
+        logging.error(str(e))
 
 
-global proxies, headers
-userAgentMiddleware = configure.userAgent.randomUserAgentMiddleware
-headers = userAgentMiddleware.getRandomHeaders()
-# 随机IP
-proxies = getRandomIP()
+def getRandomPathName():
+    """
+    获取随机数命名文件夹
+    :return:
+    """
+    return time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()) + "_" + str(uuid.uuid4())
 
-if __name__ == '__main__':
+
+def run(url):
     # 定义常量
-    imgPath = "F:\\GZH\\img"
-    htmlPath = "F:\\GZH\\"
-    newHtmlPath = "F:\\GZH\\"
-
-    url = "https://mp.weixin.qq.com/s?timestamp=1546254005&src=3&ver=1&signature=64KOvajKkM5b-oRNW0N-Foy2OKtwxDVyV58DiofRbumRAlLgMdKssCvwMw*htwxliMjBveSD3ATjXrL1IOV4DoMgoX261NC*lK0*5lztLB0P7k1DZRwsibekTLRXQPDtGwegLs-O0CCVnmCxyxbceEeqTKNwfcGMjlNZD*8pDlc="
+    rootPath = os.path.dirname(__file__) + os.path.altsep + "GZH" + os.path.altsep + getRandomPathName()
+    imgPath = rootPath + os.path.altsep + "img"
 
     # 下载HTML文件
-    htmlName = downloadHtml(url, htmlPath, headers, proxies)
+    htmlName = downloadHtml(url, rootPath)
 
     # 下载图片
-    imgList = getImgLinkList(url, headers, proxies)
-    downloadImg(imgList, headers, proxies, imgPath)
+    imgList = getImgLinkList(url)
+    downloadImg(imgList, imgPath)
 
     # 替换图片写入新的HTML
-    afterReplaceImgHtml = replaceImg(htmlPath, htmlName, imgPath)
-    writeImgToNewHTML(newHtmlPath, afterReplaceImgHtml, htmlName)
+    afterReplaceImgHtml = replaceImg(rootPath, htmlName, imgPath)
+    writeImgToNewHTML(rootPath, afterReplaceImgHtml, htmlName)
+
+
+if __name__ == '__main__':
+    global proxies, headers
+    headers = userAgent.UserAgent().getRandomHeaders()
+    # 随机IP
+    proxies = getRandomIP()
+
+    url = "https://mp.weixin.qq.com/s?__biz=MjM5NjQ1MTkyMA==&mid=2653814260&idx=1&sn=c82f1695df73ef0dfe76db6a2b2ce1f5&chksm=bd305ecb8a47d7dd64135991ac765723693161c4248b9efde8c15e4651094d7ac9146636c051&scene=0&xtrack=1#rd"
+
+    run(url)
