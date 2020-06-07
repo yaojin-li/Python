f8b031128d6cb913bdea3bd3676b75ab42459284
lixj-zj <lixj_zj@163.com>
2020-06-07 11:30:18 +0800

    add test

---
+++ algorithms/test.py
@@ -0,0 +1,16 @@
+"""
+ !/usr/bin/env python3.6
+ -*- coding: utf-8 -*-
+ --------------------------------
+ Description :
+ --------------------------------
+ @Time    : 2020/4/15 23:47
+ @File    : test.py
+ @Software: PyCharm
+ --------------------------------
+ @Author  : lixj
+ @contact : lixj_zj@163.com
+"""
+
+a = [1,2,3,4,5]
+print(a[-4:-2])
